97e6b26c839313e73a14335821cd66a510f12466
Christopher Aytona <christopher.aytona@gmail.com>
2016-01-30 07:53:16 -0500

    Power methods added

--- GBCGameJam2016/Assets/My Assets/Scripts/PlayerController.cs
+++ GBCGameJam2016/Assets/My Assets/Scripts/PlayerController.cs
@@ -4,6 +4,20 @@ using System.Collections;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+	#region Enums
+
+	/// <summary>
+	/// The type of movement states the player can be in
+	/// </summary>
+	public enum PlayerState
+	{
+		Normal,
+		Flying,
+		Phase
+	}
+
+	#endregion Enums
+
 	#region Public Variables
 
 	[Tooltip("Walking speed")]
@@ -15,10 +29,13 @@ public class PlayerController : MonoBehaviour
 	[Tooltip("Amount of force for jump")]
 	public float jumpForce;
 
+	[Tooltip("The amount of powers there are in total")]
+	public int numOfPowers;
+
 	[Tooltip("The ground check object")]
 	public Transform groundCheck;
 
-	[Tooltip("The object that contains the character sprite")]
+	[Tooltip("The object that contains the player sprite")]
 	public GameObject spriteContainer;
 
 	#endregion Public Variables
@@ -31,7 +48,12 @@ public class PlayerController : MonoBehaviour
 	private bool isOnGround = false;
 
 	/// <summary>
-	/// The character's movement direction
+	/// Array of bools to check if the player has each power
+	/// </summary>
+	private bool[] powers;
+
+	/// <summary>
+	/// The player's movement direction
 	/// </summary>
 	private Vector2 currentDirection = Vector2.one;
 
@@ -40,6 +62,11 @@ public class PlayerController : MonoBehaviour
 	/// </summary>
 	private Rigidbody2D rb2d;
 
+	/// <summary>
+	/// The current amount of jumps
+	/// </summary>
+	private int jumpCount;
+
 	#endregion Private Variables
 
 	#region MonoBehaviour
@@ -47,11 +74,12 @@ public class PlayerController : MonoBehaviour
 	void Awake()
 	{
 		rb2d = GetComponent<Rigidbody2D> ();
+		powers = new bool[numOfPowers];
 	}
 
 	void FixedUpdate()
 	{
-		CheckInput (Vector2.right * Input.GetAxis("Horizontal"));
+		BasicMovement (Vector2.right * Input.GetAxis("Horizontal"));
 	}
 
 	void Update()
@@ -61,16 +89,12 @@ public class PlayerController : MonoBehaviour
 
 	#endregion MonoBehaviour
 
-	#region Public Methods
-
-	#endregion Public Methods
-
 	#region Basic Movement Methods
 
 	/// <summary>
-	/// Checks the input of the player to set the character Orientaton and movement.
+	/// Update for basic movement methods
 	/// </summary>
-	private void CheckInput(Vector2 direction)
+	private void BasicMovement(Vector2 direction)
 	{
 		Walk (direction);
 		Jump ();
@@ -91,8 +115,8 @@ public class PlayerController : MonoBehaviour
 	{
 		if (Input.GetKeyDown (KeyCode.Space) && isOnGround) 
 		{
-			Debug.Log ("Jump");
 			rb2d.AddForce (Vector2.up * jumpForce);
+			jumpCount++;
 		}
 	}
 
@@ -103,7 +127,57 @@ public class PlayerController : MonoBehaviour
 	{
 		Collider2D collider = Physics2D.OverlapPoint(groundCheck.transform.position);
 		isOnGround = (collider != null);
+		if (isOnGround)
+			jumpCount = 0;
 	}
 
 	#endregion Basic Movement Methods
+
+	#region Power Movement Methods
+
+	/// <summary>
+	/// Update for power movement methods
+	/// </summary>
+	private void PowerMovement()
+	{
+		DoubleJump ();
+	}
+
+	/// <summary>
+	/// Power index of 0
+	/// Makes the character jump while in air
+	/// </summary>
+	private void DoubleJump()
+	{
+		if (Input.GetKeyDown (KeyCode.Space) && powers [0] && jumpCount < 2)
+			rb2d.AddForce (Vector2.up * jumpForce);
+	}
+
+	/// <summary>
+	/// Power index of 1
+	/// Teleports the player towards the mouse
+	/// </summary>
+	private void Teleport()
+	{
+
+	}
+
+	/// <summary>
+	/// Power index of 2
+	/// Makes the player enter or exit phase
+	/// </summary>
+	private void Phase()
+	{
+
+	}
+
+	/// <summary>
+	/// Power index of 3
+	/// Makes the player fly
+	/// </summary>
+	private void Fly()
+	{
+
+	}
+	#endregion Power Movement Methods
 }
