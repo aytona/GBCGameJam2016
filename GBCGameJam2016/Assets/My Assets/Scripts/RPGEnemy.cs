7b05c3c55ae3620773255e2bfae597031fa8974e
Christopher Aytona <christopher.aytona@gmail.com>
2016-01-30 19:56:24 -0500

    Merge branch 'master' into Chris
    # Conflicts:
    #	GBCGameJam2016/Library/metadata/00/0000000000000000c000000000000000

---
+++ GBCGameJam2016/Assets/My Assets/Scripts/RPGEnemy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RPGEnemy : MonoBehaviour {
+
+    public int minBaseHP = 5;
+    public int maxBaseHP = 8;
+    public int minBaseSpeed = 2;
+    public int maxBaseSpeed = 5;
+    public int minBaseAttack = 3;
+    public int maxBaseAttack = 6;
+    public int minBaseDefence = 3;
+    public int maxBaseDefence = 6;
+
+    public Stats enemyStats;
+
+    public int minLevel, maxLevel;
+
+    public int currentLevel;
+
+	// Use this for initialization
+	void Start () {
+        SetStats();
+	}
+
+    private void SetStats()
+    {
+        enemyStats.HP = Random.Range(minBaseHP, maxBaseHP) * currentLevel;
+        enemyStats.speed = Random.Range(minBaseSpeed, maxBaseSpeed) * currentLevel;
+        enemyStats.attack = Random.Range(minBaseAttack, maxBaseAttack) * currentLevel;
+        enemyStats.defence = Random.Range(minBaseDefence, maxBaseDefence) * currentLevel;
+    }
+}
